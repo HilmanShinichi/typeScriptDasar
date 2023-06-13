67de372eef707605b43c4d0b9a6c8c60e259116a
Hilman Shinichi <hilmanseptiana19@gmail.com.com>
2023-06-13 21:22:20 +0700

    typescript dasar

---
+++ hello.ts
@@ -0,0 +1,8 @@
+function sayhello(nama: string) {
+    return `Nama Saya Adalah ${nama}`
+}
+
+let res: string = sayhello("hilman");
+
+
+console.log(res)
---
+++ tipedata.ts
@@ -0,0 +1,18 @@
+//Error 
+// isMarried = "hilman";
+
+let age: number = 20;
+
+// Error
+// age = true; 
+// age = "Hilman";
+
+let firstName: string = "Hilman";
+let lastName: string = "Septiana";
+let fullName: string = `${firstName} ${lastName}`
+
+console.log(fullName)
+
+
+// Error 
+// lastName = true
